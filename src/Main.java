6a6ae021ce0dbd9d055df6640014f9ddb94f1bce
Hlib <pavlikgo78@gmail.com>
2024-10-28 17:46:04 +0200

    feat: edit start up file with text utils class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringBuffer textBuffer;
+        String[] words;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Input text with validation
+            textBuffer = TextUtils.inputText(scanner);
+
+            // Input words array with validation
+            words = TextUtils.inputWords(scanner);
+
+            // Perform the analysis
+            int[] occurrences = TextUtils.countWordOccurrencesInSentences(textBuffer, words);
+
+            // Output the results
+            for (int i = 0; i < words.length; i++) {
+                System.out.println("Word '" + words[i] + "' occurs in " + occurrences[i] + " sentences.");
+            }
+
+        } catch (Exception e) {
+            System.err.println("An error occurred during text analysis: " + e.getMessage());
+        }
     }
 }
